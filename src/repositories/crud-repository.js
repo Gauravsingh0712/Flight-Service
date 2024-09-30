1b15b64274930d9fa21c87bf2d948f2fb83f069c
Gaurav Singh <sumitgaurav6295@gmail.com>
2024-09-30 22:06:41 +0530

    added get all airplane api

--- src/repositories/crud-repository.js
+++ src/repositories/crud-repository.js
@@ -6,62 +6,36 @@ class CrudRepository {
     }
 
     async create(data) {
-        try {
-            const response = await this.model.create(data);
-            return response;
-        } catch (error) {
-            console.log(error)
-            logger.error("Something went wrong in the crud repo : create");
-            throw error;
-        }
+        const response = await this.model.create(data);
+        return response;
     }
 
     async destroy(data) {
-        try {
-            const response = await this.model.destroy({
-                where: {
-                    id: data
-                }
-            });
-            return response;
-        } catch (error) {
-            logger.error("Something went wrong in the crud repo : destroy");
-            throw error;
-        }
+        const response = await this.model.destroy({
+            where: {
+                id: data
+            }
+        });
+        return response;
     }
 
     async get(data) {
-        try {
-            const response = await this.model.findByPk(data);
-            return response;
-        } catch (error) {
-            logger.error("Something went wrong in the crud repo : get");
-            throw error;
-        }
+        const response = await this.model.findByPk(data);
+        return response;
     }
 
     async getAll() {
-        try {
-            const response = await this.model.findAll();
-            return response;
-        } catch (error) {
-            logger.error("Something went wrong in the crud repo : getall");
-            throw error;
-        }
+        const response = await this.model.findAll();
+        return response;
     }
 
     async update(id, data) { //data->{col:val,...}
-        try {
-            const response = await this.model.update(data, {
-                where: {
-                    id: id
-                }
-            });
-            return response;
-        } catch (error) {
-            logger.error("Something went wrong in the crud repo : update");
-            throw error;
-        }
+        const response = await this.model.update(data, {
+            where: {
+                id: id
+            }
+        });
+        return response;
     }
 }
 
--- src/routes/v1/airplane-route.js
+++ src/routes/v1/airplane-route.js
@@ -4,10 +4,13 @@ const { AirplaneController } = require('../../controllers')
 const { AirplaneMiddlewares } = require('../../middlewares')
 const router = express.Router()
 
-//POST REQUEST
+// api/v1/airplanes
 // First it will validate through middleware and then call the controller function
 router.post('/',
     AirplaneMiddlewares.validateCreateRequest,
     AirplaneController.createAirplane)
+
+// api/v1/airplanes
+router.get('/', AirplaneController.getAirplanes)
 
 module.exports = router;
