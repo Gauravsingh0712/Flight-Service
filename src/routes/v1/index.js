0f2b91d7a9d1052ede4449c2c10b4e2bab2cd008
Gaurav Singh <sumitgaurav6295@gmail.com>
2024-10-02 00:31:09 +0530

    created create city api

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -3,8 +3,10 @@ const express = require('express')
 const router = express.Router()
 const { InfoController } = require('../../controllers')
 const airplaneRoutes = require('./airplane-route')
+const cityRoutes = require('./city-route')
 
 router.use('/airplanes', airplaneRoutes)
+router.use('/cities', cityRoutes)
 router.get('/info', InfoController.info)
 
 module.exports = router;
