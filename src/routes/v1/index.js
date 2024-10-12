8027bc6de81c5bfa26b5c967663fa3542e97ef5e
Gaurav Singh <sumitgaurav6295@gmail.com>
2024-10-12 23:32:57 +0530

    FEAT: added create flight api

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -5,10 +5,12 @@ const { InfoController } = require('../../controllers')
 const airplaneRoutes = require('./airplane-route')
 const cityRoutes = require('./city-route')
 const airportRoutes = require('./airport-route')
+const flightRoutes = require('./flight-route')
 
 router.use('/airplanes', airplaneRoutes)
 router.use('/cities', cityRoutes)
 router.use('/airports', airportRoutes)
+router.use('/flights', flightRoutes)
 router.get('/info', InfoController.info)
 
 module.exports = router;
