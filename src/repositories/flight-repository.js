dfcd02e2f2e8c48d78d384f6d8bf5d6b157a5fa4
Gaurav Singh <sumitgaurav6295@gmail.com>
2024-10-13 20:18:20 +0530

    FEAT: added get flights api

--- src/repositories/flight-repository.js
+++ src/repositories/flight-repository.js
@@ -5,5 +5,13 @@ class FlightRepository extends CrudRepository {
     constructor() {
         super(Flight)
     }
+
+    async getAllFights(filter) {
+        const response = await Flight.findAll({
+            where: filter,
+            order: sort
+        })
+        return response;
+    }
 }
 module.exports = FlightRepository;
